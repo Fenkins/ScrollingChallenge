fc3c5d3eb510998588af8a2fe7561db9b7cf1e73
Anatoly Kovalev <inatoley@gmail.com>
2015-10-02 17:59:55 +0300

    scrolling challange

---
+++ ScrollingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollingChallenge
+//
+//  Created by Fenkins on 02/10/15.
+//  Copyright © 2015 Fenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
